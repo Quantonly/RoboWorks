a839e4b5fc2c08e5a8e6f4f382ebc5e1cd1f6ad5
Quantonly <quantonly.dev@gmail.com>
2022-02-04 17:10:18 +0100

    Sign in page functionality

--- lib/pages/authentication/sign_in.dart
+++ lib/pages/authentication/sign_in.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:form_field_validator/form_field_validator.dart';
+
+import 'package:provider/provider.dart';
+
+import 'package:robo_works/services/authentication.dart';
+import 'package:robo_works/globals/style.dart' as style;
 
 class SignInPage extends StatefulWidget {
   const SignInPage({Key? key}) : super(key: key);
@@ -8,14 +14,134 @@ class SignInPage extends StatefulWidget {
 }
 
 class _SignInPageState extends State<SignInPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  void login(String method) async {
+    context
+        .read<AuthenticationService>()
+        .signIn(
+          email: emailController.text.trim(),
+          password: passwordController.text.trim(),
+        )
+        .then((res) => {
+              if (res['error'] != null)
+                {
+                  // ignore: avoid_print
+                  print(res['error'])
+                },
+            });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
       body: Container(
         color: Colors.black87,
-        child: const Center(
-          child: Text('Sign In'),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Center(
+                  child: Row(
+                    children: <Widget>[
+                      Align(
+                        alignment: Alignment.bottomCenter,
+                        child: SizedBox(
+                          height: 70,
+                          width: MediaQuery.of(context).size.width,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  transform: Matrix4.translationValues(0.0, -30.0, 0.0),
+                  child: const Center(
+                    child: Text(
+                      'Sign In',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Form(
+                  key: _formKey,
+                  child: Column(children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                      ),
+                      child: TextFormField(
+                        controller: emailController,
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        decoration: style.getTextFieldDecoration('Email'),
+                        validator: MultiValidator([
+                          RequiredValidator(errorText: 'Invalid email'),
+                          EmailValidator(errorText: 'Invalid email'),
+                        ]),
+                        keyboardType: TextInputType.emailAddress,
+                        style: const TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 10,
+                      ),
+                      child: TextFormField(
+                        controller: passwordController,
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        decoration: style.getTextFieldDecoration('Password'),
+                        validator: RequiredValidator(
+                            errorText: 'Password is required'),
+                        style: const TextStyle(
+                          color: Colors.white,
+                        ),
+                        obscureText: true,
+                      ),
+                    ),
+                  ]),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    if (_formKey.currentState!.validate()) {
+                      login('signin');
+                    }
+                  },
+                  child: Container(
+                    height: 50,
+                    margin: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 10,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      color: Colors.white,
+                    ),
+                    child: const Center(
+                      child: Text(
+                        'Sign In',
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
