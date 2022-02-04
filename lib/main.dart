b3c1617da84d7d808f50b61ad08f639c2d61c261
Cody Volz <quantonly.dev@gmail.com>
2022-02-04 16:20:21 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const RoboWorks());
+}
+
+class RoboWorks extends StatelessWidget {
+  const RoboWorks({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'RoboWorks',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Dashboard(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class Dashboard extends StatefulWidget {
+  const Dashboard({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[],
+        ),
+      ),
+    );
+  }
+}
